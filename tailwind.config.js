e37838c45cddb88573a88ba2bf5b6cda267f3fb9
matdsoupe <matheus_pessanha2001@outlook.com>
2021-09-23 15:06:00 -0300

    feat: color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
   },
   darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      blue: {
+        100: '#BCE0EF',
+        300: '#88B8CC',
+        500: '#277DA1',
+        700: '#0E4771',
+      },
+      green: '#25CE52',
+      orange: '#F8961E',
+      yellow: '#F5BD00',
+      red: '#FF635D',
+    },
     fontSize: {
       xs: '.75rem',
       sm: '.875rem',
